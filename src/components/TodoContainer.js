09a4779f15cb3b711360824ae4f8e7e866fdf3a3
Ntwali-Josue <kntwalijos@gmail.com>
2021-11-29 17:43:49 +0200

    Create TodoContainer component

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class TodoContainer extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Helloooo</h1>
+        <p>I am in React component</p>
+      </div>
+    )
+  }
+}
+
+export default TodoContainer;
