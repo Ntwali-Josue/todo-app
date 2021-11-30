c790886c4eb5c21af1977bca234d1a619eda69ab
Ntwali-Josue <kntwalijos@gmail.com>
2021-11-30 11:10:29 +0200

    Validate input field

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -14,17 +14,28 @@ class InputTodo extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    this.props.addTodoProps(this.state.title);
-    this.setState({
-      title: ""
-    })
+    if (this.state.title.trim()) {
+      this.props.addTodoProps(this.state.title)
+      this.setState({
+        title: "",
+      })
+    } else {
+      alert("Please write item")
+    }
   }
 
   render() {
     return (
-      <form onSubmit={this.state.title}>
-        <input type="text" placeholder="Add Todo..." value={this.state.title} onChange={this.onChange}/>
-        <button>Submit</button>
+      <form onSubmit={this.handleSubmit} className="form-container">
+        <input
+          type="text"
+          className="input-text"
+          placeholder="Add todo..."
+          value={this.state.title}
+          name="title"
+          onChange={this.onChange}
+        />
+        <button className="input-submit">Submit</button>
       </form>
     )
   }
